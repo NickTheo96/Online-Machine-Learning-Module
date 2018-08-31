542108b8961a7a40c86608b060911e5486006b10
NickTheo96 <nicktheodoulou96@gmail.com>
2018-08-31 21:07:14 +0100

    HMM section correct

--- Coursework/Main.m
+++ Coursework/Main.m
@@ -1,14 +1,14 @@
 One_a = (0.4 * 0.7 * 0.6 ) *( 0.1*0.4*0.4);
 
-M = [0.3 0.7; 0.6 0.4 ];
-B = [0.2 0.3 0.4 0.1; 0.4 0.1 0.2 0.3];
-P = [0.4 ;0.6];
-v = [4 1 3];
+%M = [0.3 0.7; 0.6 0.4 ];
+%B = [0.2 0.3 0.4 0.1; 0.4 0.1 0.2 0.3];
+%P = [0.4 ;0.6];
+%v = [4 1 3];
 
-%B = dlmread("B.txt");
-%M = dlmread("M.txt");
-%P = dlmread("p.txt");
-%v = dlmread("v.txt");
+B = dlmread("B.txt");
+M = dlmread("M.txt");
+P = dlmread("p.txt");
+v = dlmread("v.txt");
 
 alpha = alpha_dynamic(M, B, P, v);
 beta=beta_dynamic(M, B, P, v);
