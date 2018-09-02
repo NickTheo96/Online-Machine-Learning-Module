28bf0b5c2817e2e02f208d7fa82c5a9a9949bd17
NickTheo96 <nicktheodoulou96@gmail.com>
2018-09-02 11:33:34 +0100

    -Getting suitable values for gamma between 0 and 1 :bug was found when calculating g(1) and g(0)

--- Coursework/Aggregating Algorithm/AA.m
+++ Coursework/Aggregating Algorithm/AA.m
@@ -1,50 +1,51 @@
-data_matrix = dlmread("tennis1.txt");
+data_matrix = dlmread("tennis1.txt");%load in the data into a matrix
 tennis_data(:,1) = data_matrix(:,4);
 tennis_data(:,2) = data_matrix(:,6);
 tennis_data(:,3) = data_matrix(:,8);
-tennis_data(:,4) = data_matrix(:,10);
+tennis_data(:,4) = data_matrix(:,10);%create a new matrix of the 4 columns actually used
 
 weights = [0.25,0.25,0.25,0.25];%initialise weights
 
 
-    length_tennis_data = length(tennis_data);
+    length_tennis_data = length(tennis_data);%used to define length of loops
     length_weights = length(weights);
-    sum_weights=0;
-    g_0=0;
+    sum_weights=0;%initialise the sum of the weights to zero
+    g_0=0;%initialise gammas
     g_1=0;
-    ita = 2;
+    ita = 2;%set ita to 2
     p =zeros(1,length_weights);%initialize the p matrix 
-    gamma=zeros(length_tennis_data,1);
+    gamma=zeros(length_tennis_data,1);%initialize gamma vector which is what is calculated using the AA
     
-for t = 1:588
+for t = 1:length_tennis_data
     
 
     for n = 1:length(weights)
-       sum_weights = sum_weights + weights(n);
+       sum_weights = sum_weights + weights(n);%sum the weights on each iteration
     end
 
     for n = 1:length(weights)
-        p(n) = weights(n)/sum_weights;
+        p(n) = weights(n)/sum_weights;%normalise the distribution using the expert on each iteration
     end
     
     for n = 1:length(weights)
-    g_0= g_0 + (log(p(n)) * exp(-ita*(tennis_data(t,n)^2)));
-    g_1= g_1 + (log(p(n)) * exp(-ita*(1-tennis_data(t,n)^2)));
+    g_0= g_0 + (p(n)) * exp(-ita*(tennis_data(t,n)^2));%calculate the summation in the g(0) and g(1)
+    %equation fist
+    g_1= g_1 + (p(n)) * exp(-ita*(1-tennis_data(t,n)^2));
     end
     
-    g_0=(-1/ita) * g_0;
-    g_1=(-1/ita) * g_1;
+    g_0=(-1/ita) * log(g_0);%calculate g(0) and g(1)
+    g_1=(-1/ita) * log(g_1);
     
-    gamma(t)=0.5-((0.5)*(g_1-g_0));
+    gamma(t)=0.5-((0.5)*(g_1-g_0));%calculate gamma for each iteration
     
     
     for n = 1:length(weights)
-        weights(n)=weights(n)*exp((-ita*(1-tennis_data(t,n)^2)));
+        weights(n)=weights(n)*exp((-ita*(1-tennis_data(t,n)^2)));%update the weights for the next iteration
     end
     
-    g_0=0;
+    g_0=0;%set g(0) to zero so that this can be calculated each iteration
     g_1=0;
-    sum_weights=0;
+    sum_weights=0;%set sum of weight to zero so that they can be normalised correctly
     
 end
     
